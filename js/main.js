c3bd5884f019c9f223122d83b36d25d1bc05463c
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-29 16:58:16 +0100

    struttura con tutte le card

--- js/main.js
+++ js/main.js
@@ -36,3 +36,34 @@ const teamMembers = [
 		img: "img/female3.png",
 	},
 ];
+
+const teamCardsGroup = document.getElementById("teamCardsGroup");
+let cardsHtml = ``;
+
+teamMembers.forEach((member) => {
+	const memberCard = `
+        <div class="col">
+            <div class="card bg-dark bg-gradient text-white h-100">
+                <div class="row g-0 h-100">
+                    <div class="col-md-4">
+                        <img
+                            src="./assets/${member.img}"
+                            class="img-fluid rounded-start"
+                            alt="${member.name} + ${member.role}"
+                        />
+                    </div>
+                    <div class="col-md-8 h-100">
+                        <div class="p-2 h-100">
+                            <h5 class="card-title text-uppercase">${member.name}</h5>
+                            <p class="card-text">${member.role}</p>
+                            <p class="card-text fs-6 text-primary">
+                                ${member.email}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+	cardsHtml += memberCard;
+});
+teamCardsGroup.innerHTML = cardsHtml;
